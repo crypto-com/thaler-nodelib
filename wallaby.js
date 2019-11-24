8d59c2878233126e401c38c9c6172f68b1b3db0e
Calvin Lau <calvinlauco@gmail.com>
2019-11-24 20:15:02 +0800

    Problem: (CRO-552) Missing project setup
    Solution: Setup Typescript and neon project environment

---
+++ wallaby.js
@@ -0,0 +1,19 @@
+module.exports = function(_wallaby) {
+    return {
+        files: [
+            "native/index.node",
+            "lib/src/**/*.ts",
+            {
+                pattern: "lib/src/**/*.spec.ts",
+                ignore: true,
+            }, 
+        ],
+        tests: ["lib/src/**/*.spec.ts"],
+        testFramework: "mocha",
+        env: {
+            type: "node",
+            runner: "node",
+        },
+        workers: {recycle: true},
+    };
+};
